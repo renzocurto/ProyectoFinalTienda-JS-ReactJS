2406b92f02dbfbe43820b0a539078cbb60c704ee
Renzo Curto <renzocurto9@gmail.com>
2022-04-14 20:13:51 -0300

    Primera App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 
 function App() {
   return (
-      <h1>Primera App creada para el curso</h1>
+    <div>
+    <h1>Desafío Nº 1</h1>
+    <h2>Entrega del primer desafío del curso de React</h2>
+    <p>Renzo Curto</p>
+    </div>
   );
 }
 
