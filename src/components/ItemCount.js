920d5f0b7e67af445935a0ff6b2f649cb821dab6
Renzo Curto <renzocurto9@gmail.com>
2022-05-14 18:53:10 -0300

    Desafio6

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,12 +3,12 @@ import React, { useState } from "react"
  const ItemCount = ({ stock, onAdd }) => {
  	const [count, setCount] = useState(0)
 
- 	const handleAdd = () => {
+ 	const addItem = () => {
  		if (count < stock) {
  			setCount(count + 1)
  		}
  	}
- 	const handleRemove = () => {
+ 	const removeItem = () => {
  		if (count > 0) {
  			setCount(count - 1)
  		}
@@ -18,11 +18,11 @@ import React, { useState } from "react"
  		<>
  			<div className="flex gap-4 mt-4">
  				<div className="flex flex-row">
- 					<button className="btn " onClick={handleRemove}>
+ 					<button className="btn " onClick={removeItem}>
  						<i className="fas fa-minus">-</i>
  					</button>
  					<label className="alert alert-white">{count}</label>
- 					<button className="btn " onClick={handleAdd}>
+ 					<button className="btn " onClick={addItem}>
  						<i className="fas fa-plus">+</i>
  					</button>
  				</div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     </div>
   </div>
   <div class="navbar-center">
-    <Link to='/' class="btn btn-ghost normal-case text-xl">Desafío Nº 5</Link>
+    <Link to='/' class="btn btn-ghost normal-case text-xl">Desafío Nº 6 - Sincronizar Counter</Link>
   </div>
   <div class="navbar-end">
 
